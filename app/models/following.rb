9763dc9ad39bc0bf8e1acaa092b2f9829e9a2c3c
Vishesh <vishesh.modi@atharvasystem.com>
2020-03-18 18:24:15 +0530

    Add class to follow user and topic

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,21 +1,21 @@
 class Following < ApplicationRecord
   belongs_to :followable, polymorphic: true
   
-  validate :can_not_follow_own
   validate :already_following
+  validate :can_not_follow_own
   
   private
-  def can_not_follow_own
-   case followable_type
-     when 'User'
-       self.errors[:base] << 'Could Not follow Own' if followed_id.eql?(followed_id)
-     when 'Topic'
-       self.errors[:base] << 'Could Not follow Own Topic' if followed_id.eql?(followed_id)
-   end
-  end
-  
   def already_following
     following = Following.exists?(followable_id: followable_id, followed_id: followed_id, followable_type: followable_type)
     self.errors[:base] << 'Already following.' if following
+  end
+
+  def can_not_follow_own
+    case followable_type
+      when 'User'
+        self.errors[:base] << 'Could Not follow Own' if followed_id.eql?(followed_id)
+      when 'Topic'
+        self.errors[:base] << 'Could Not follow Own Topic' if followed_id.eql?(followed_id)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   
   has_many :followers, foreign_key: :follower_id, class_name: 'Following'
   has_many :followeds, foreign_key: :followed_id, class_name: 'Following'
-  belongs_to :followable, polymorphic: true
+  has_many :following, foreign_key: :followed_id, class_name: 'Following'
 end
